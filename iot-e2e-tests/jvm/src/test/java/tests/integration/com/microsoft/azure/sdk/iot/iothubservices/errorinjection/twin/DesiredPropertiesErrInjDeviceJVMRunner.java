29425b3c58bfe634580bef5ba560590fc903ca6d
timtay-microsoft <timtay@microsoft.com>
2019-04-10 11:20:06 -0700

    more fixes

--- iot-e2e-tests/jvm/src/test/java/tests/integration/com/microsoft/azure/sdk/iot/iothubservices/errorinjection/twin/DesiredPropertiesErrInjDeviceJVMRunner.java
+++ iot-e2e-tests/jvm/src/test/java/tests/integration/com/microsoft/azure/sdk/iot/iothubservices/errorinjection/twin/DesiredPropertiesErrInjDeviceJVMRunner.java
@@ -21,13 +21,13 @@ import java.util.Collection;
 @RunWith(Parameterized.class)
 public class DesiredPropertiesErrInjDeviceJVMRunner extends DesiredPropertiesErrInjTests
 {
-    public DesiredPropertiesErrInjDeviceJVMRunner(String deviceId, String moduleId, IotHubClientProtocol protocol, AuthenticationType authenticationType, ClientType clientType, String publicKeyCert, String privateKey, String x509Thumbprint)
+    public DesiredPropertiesErrInjDeviceJVMRunner(IotHubClientProtocol protocol, AuthenticationType authenticationType, ClientType clientType, String publicKeyCert, String privateKey, String x509Thumbprint)
     {
-        super(deviceId, moduleId, protocol, authenticationType, clientType, publicKeyCert, privateKey, x509Thumbprint);
+        super(protocol, authenticationType, clientType, publicKeyCert, privateKey, x509Thumbprint);
     }
 
     //This function is run before even the @BeforeClass annotation, so it is used as the @BeforeClass method
-    @Parameterized.Parameters(name = "{2} with {3} auth using {4}")
+    @Parameterized.Parameters(name = "{0} with {1} auth using {2}")
     public static Collection inputsCommons() throws Exception
     {
         iotHubConnectionString = Tools.retrieveEnvironmentVariableValue(TestConstants.IOT_HUB_CONNECTION_STRING_ENV_VAR_NAME);
